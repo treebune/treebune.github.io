eb28194e8d71e2e91311968446ef185b6a713748
treebune <treebune@gmail.com>
2026-06-01 16:57:05 +0800

    Update toc-name.js

--- js/toc-name.js
+++ js/toc-name.js
@@ -1,57 +1,18 @@
 var SERIES_NAME = {
   "key": "name",
   "name": "名字",
-  "total": 10,
+  "total": 11,
   "chapters": [
-    {
-      "num": 1,
-      "title": "名字・之一",
-      "file": "01.html"
-    },
-    {
-      "num": 2,
-      "title": "名字・之二",
-      "file": "02.html"
-    },
-    {
-      "num": 3,
-      "title": "名字・之三",
-      "file": "03.html"
-    },
-    {
-      "num": 4,
-      "title": "名字・之四",
-      "file": "04.html"
-    },
-    {
-      "num": 5,
-      "title": "名字・之五",
-      "file": "05.html"
-    },
-    {
-      "num": 6,
-      "title": "名字・之七",
-      "file": "06.html"
-    },
-    {
-      "num": 7,
-      "title": "名字・之六",
-      "file": "07.html"
-    },
-    {
-      "num": 8,
-      "title": "名字・之八",
-      "file": "08.html"
-    },
-    {
-      "num": 9,
-      "title": "名字・之九",
-      "file": "09.html"
-    },
-    {
-      "num": 10,
-      "title": "名字・之十",
-      "file": "10.html"
-    }
+    {"num": 0, "title": "名字・全", "file": "all.html"},
+    {"num": 1, "title": "名字・之一", "file": "01.html"},
+    {"num": 2, "title": "名字・之二", "file": "02.html"},
+    {"num": 3, "title": "名字・之三", "file": "03.html"},
+    {"num": 4, "title": "名字・之四", "file": "04.html"},
+    {"num": 5, "title": "名字・之五", "file": "05.html"},
+    {"num": 6, "title": "名字・之七", "file": "06.html"},
+    {"num": 7, "title": "名字・之六", "file": "07.html"},
+    {"num": 8, "title": "名字・之八", "file": "08.html"},
+    {"num": 9, "title": "名字・之九", "file": "09.html"},
+    {"num": 10, "title": "名字・之十", "file": "10.html"}
   ]
 };
